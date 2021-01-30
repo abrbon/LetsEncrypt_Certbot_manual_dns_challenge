436712e25888368e55aa858ea023a3b27a0cfe80
abrbon <abrbon@gmail.com>
2021-01-31 00:26:29 +0100

    Update certbot_config_vars.sh

--- certbot_config_vars.sh
+++ certbot_config_vars.sh
@@ -6,34 +6,34 @@
 #
 
 # DuckDNS vars
-TOKEN="<YOUR_DUCKDNS_TOKEN_HERE>"					# DuckDNS Token.
-DOMAIN="<YOUR_DOMAIN_HERE>"							# Domain registered at DuckDNS.
-SUB_DOMAIN_NAME="<YOUR_SUBDOMAIN_HERE>"				# Subdomain, this is the name you would like to create certs for.
+TOKEN="<YOUR_DUCKDNS_TOKEN_HERE>"			# DuckDNS Token.
+DOMAIN="<YOUR_DOMAIN_HERE>"				# Domain registered at DuckDNS.
+SUB_DOMAIN_NAME="<YOUR_SUBDOMAIN_HERE>"			# Subdomain, this is the name you would like to create certs for.
 
 # Certbot vars
-LOGDIR="/usr/local/certbot_files/logs"				# Logging directory.
-CERTBOT_WRKDIR="/usr/local/certbot_files"			# Working directory for all scripts to work from.
-STAGING="--test-cert"								# Use '--test-cert' for testing first, leave blank for real certificates.
-DRY_RUN="--dry-run"									# Use '--dry-run' to test first, no certs will be created in this mode.
-													# Leave blank to disable dry-run.
+LOGDIR="/usr/local/certbot_files/logs"			# Logging directory.
+CERTBOT_WRKDIR="/usr/local/certbot_files"		# Working directory for all scripts to work from.
+STAGING="--test-cert"					# Use '--test-cert' for testing first, leave blank for real certificates.
+DRY_RUN="--dry-run"					# Use '--dry-run' to test first, no certs will be created in this mode.
+							# Leave blank to disable dry-run.
 
-AUTH_HOOK="duckdns_auth-hook.sh"					# Call this script to authenticate to DuckDNS to create a temporary TXT
-													# record for validating LetsEncrypt via DNS challenge.
+AUTH_HOOK="duckdns_auth-hook.sh"			# Call this script to authenticate to DuckDNS to create a temporary TXT
+							# record for validating LetsEncrypt via DNS challenge.
 
-CLEANUP_HOOK="duckdns_cleanup-hook.sh"				# Call this script to cleanup the temporary TXT record previously made.
+CLEANUP_HOOK="duckdns_cleanup-hook.sh"			# Call this script to cleanup the temporary TXT record previously made.
 
-DEPLOY_HOOK="duckdns_deploy-hook.sh"				# Call this script to actually deploy the generated certificates and
-													# copy them to the necessary location.
+DEPLOY_HOOK="duckdns_deploy-hook.sh"			# Call this script to actually deploy the generated certificates and
+							# copy them to the necessary location.
 
 
 REGISTER_EMAIL="--register-unsafely-without-email"	# Email used for registration and recovery contact.
-													# You will be able to receive notice about
-													# impending expiration or revocation of your
-													# certificates or problems with your Certbot
-													# installation that will lead to failure to renew.
-													# Not really necessary to register, if you don't want to,
-													# use the '--register-unsafely-without-email' option here.
-													# REGISTER_EMAIL="-m youremailhere@provider.com"
+							# You will be able to receive notice about
+							# impending expiration or revocation of your
+							# certificates or problems with your Certbot
+							# installation that will lead to failure to renew.
+							# Not really necessary to register, if you don't want to,
+							# use the '--register-unsafely-without-email' option here.
+							# REGISTER_EMAIL="-m youremailhere@provider.com"
 
 # SSH vars (like locations where the ssl certs should be copied)
 SSL_PATH="/etc/ssl"
